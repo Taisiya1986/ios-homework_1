667e0a749722716d89fdc77035300433fb8254f5
Taisiya Kutyavina <kutyavinatn@gmail.com>
2022-09-29 19:29:19 +0300

    homework commit2

--- Navigation_1/FeedViewController.swift
+++ Navigation_1/FeedViewController.swift
@@ -6,22 +6,28 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray
-        
+        view.addSubview(postButton)
+        setupPostButton()
+    }
+    
+    let postButton: UIButton = {
         let uiButton = UIButton()
-        self.view.addSubview(uiButton)
         uiButton.setTitle("Перейти на пост", for: .normal)
         uiButton.translatesAutoresizingMaskIntoConstraints = false
-        uiButton.addAction(UIAction(handler: {_ in
-            let vc = PostViewController()
-            self.navigationController?.pushViewController(vc, animated: true)
-            vc.titlePost = self.post.title
-        }), for: .touchUpInside)
-        
-        NSLayoutConstraint.activate([
-            uiButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            uiButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            uiButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            uiButton.heightAnchor.constraint(equalToConstant: 30)
-        ])
+        uiButton.addTarget(FeedViewController?.self, action: #selector(addPostButtonAction), for: .touchUpInside)
+        return uiButton
+    }()
+    
+    func setupPostButton() {
+        postButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        postButton.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        postButton.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        postButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    }
+    
+    @objc func addPostButtonAction() {
+        let vc = PostViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+        vc.titlePost = self.post.title
     }
 }
